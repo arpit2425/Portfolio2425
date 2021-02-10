0c8d42de5be31359541ff9d73e158fd4f89adbf3
arpit2425 <arpittrivedi2425@gmail.com>
2021-02-11 00:15:18 +0530

    Code clean up

---
+++ index.js
@@ -0,0 +1,32 @@
+const proxyurl = 'https://cors-anywhere.herokuapp.com/'
+
+const url =
+  'https://cors-anywhere.herokuapp.com/https://us-central1-otp-react-native-4e01f.cloudfunctions.net/Mailer'
+const submitbtn = document.querySelector('.submit-btn')
+submitbtn.addEventListener('click', async event => {
+  event.preventDefault()
+  const fullname = document.querySelector('.fullname').value
+  const email = document.querySelector('.email').value
+  const message = document.getElementById('message').value
+  if (!fullname || !email || !message) {
+    return alert('fields cannot be empty')
+  }
+  submitbtn.textContent = 'Sending mail...'
+  try {
+    const mail = await axios.post(url, {
+      subject: fullname,
+      from: email,
+      message
+    })
+    submitbtn.textContent = 'Mail Sent'
+    document.querySelector('.fullname').value = ''
+    document.querySelector('.email').value = ''
+    document.getElementById('message').value = ''
+    submitbtn.disabled = true
+
+    console.log(mail)
+  } catch (err) {
+    console.log(err)
+    submitbtn.textContent = 'Failed to send the mail'
+  }
+})
